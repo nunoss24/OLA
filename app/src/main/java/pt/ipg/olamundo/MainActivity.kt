5cb2b5df2c4df7b46cade15024ef84dfc5436ed5
Nuno <nunoabelss2424@gmail.com>
2021-04-21 10:20:06 +0100

    Alterando o layout para linear

---
+++ app/src/main/java/pt/ipg/olamundo/MainActivity.kt
@@ -0,0 +1,14 @@
+package pt.ipg.olamundo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun enviaMensagem(view: View) {}
+}
