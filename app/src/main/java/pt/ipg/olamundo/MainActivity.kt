3611563bd3ae40109aed11f1c0657c706345d7c0
nunoss24 <nunoabelss2424@gmail.com>
2021-04-21 10:50:58 +0100

    mostra atividade

--- app/src/main/java/pt/ipg/olamundo/MainActivity.kt
+++ app/src/main/java/pt/ipg/olamundo/MainActivity.kt
@@ -1,8 +1,10 @@
 package pt.ipg.olamundo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +12,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun enviaMensagem(view: View) {}
+    fun enviaMensagem(view: View) {
+        val editTextMensagem = findViewById<EditText>(R.id.editTextMensagem)
+
+        val mensagem = editTextMensagem.text.toString()
+
+        val intent = Intent(this, MostraMensagemActivity::class.java)
+        startActivity(intent)
+
+    }
 }
