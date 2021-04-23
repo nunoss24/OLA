e37fa84e661269cedc3e31c267004e4e4062b1e6
nunoss24 <nunoabelss2424@gmail.com>
2021-04-23 09:24:14 +0100

    aparece a mensagem do outro lado

--- app/src/main/java/pt/ipg/olamundo/MostraMensagemActivity.kt
+++ app/src/main/java/pt/ipg/olamundo/MostraMensagemActivity.kt
@@ -2,10 +2,17 @@ package pt.ipg.olamundo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MostraMensagemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mostra_mensagem)
+
+        val mensagem = intent.getStringExtra("MENSAGEM")
+
+        val textViewMensagem = findViewById<TextView>(R.id.textView)
+        textViewMensagem.setText(mensagem)
+
     }
 }
