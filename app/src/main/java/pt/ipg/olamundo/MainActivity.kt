a726abd13cc0896fdae46eaf7a09f0e7c6b27e63
nunoss24 <nunoabelss2424@gmail.com>
2021-04-28 09:36:21 +0100

    mostrar data e hora noutra atividade

--- app/src/main/java/pt/ipg/olamundo/MainActivity.kt
+++ app/src/main/java/pt/ipg/olamundo/MainActivity.kt
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     companion object{
-        public val INFO_EXTRA_MENSAGEM="MENSAGEM"
+        const val INFO_EXTRA_MENSAGEM="MENSAGEM"
+        const val INFO_EXTRA_DATA="DATA"
+
     }
 
 
@@ -17,18 +20,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
+
+
     fun enviaMensagem(view: View) {
         val editTextMensagem = findViewById<EditText>(R.id.editTextMensagem)
-
         val mensagem = editTextMensagem.text.toString()
+
+
 
         if(mensagem.trim().isBlank()){
             editTextMensagem.error=getString(R.string.Mensagem_vazia)
             return
         }
 
+        //obter data e hora do sistema
+        val data = Date()
+
         val intent = Intent(this, MostraMensagemActivity::class.java)
         intent.putExtra(INFO_EXTRA_MENSAGEM, mensagem)
+        intent.putExtra(INFO_EXTRA_DATA, data)
         startActivity(intent)
 
     }
--- app/src/main/java/pt/ipg/olamundo/MostraMensagemActivity.kt
+++ app/src/main/java/pt/ipg/olamundo/MostraMensagemActivity.kt
@@ -3,6 +3,7 @@ package pt.ipg.olamundo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import java.util.*
 
 class MostraMensagemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,9 +11,13 @@ class MostraMensagemActivity : AppCompatActivity() {
         setContentView(R.layout.activity_mostra_mensagem)
 
         val mensagem = intent.getStringExtra(MainActivity.INFO_EXTRA_MENSAGEM)
+        val data = intent.getSerializableExtra(MainActivity.INFO_EXTRA_DATA) as Date
 
         val textViewMensagem = findViewById<TextView>(R.id.textView)
         textViewMensagem.setText(mensagem)
+
+        val textViewDataHora = findViewById<TextView>(R.id.textView3)
+        textViewDataHora.setText((data.toString()))
 
     }
 }
